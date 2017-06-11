46707940686b68c9be9f1c61b66451ca2ea22943
MrClan <MrClan@HomeAsusPC>
2017-06-12 05:21:48 +0545

    Add project files.

---
+++ AspIdentityPracticeConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace AspIdentityPracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContext ctx = new IdentityDbContext("DefaultConnectionString");
+            //ctx.Database.Connection.Open();
+
+
+
+            var username = "mrclan";
+            var password = "apple_mango4321";
+
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var creationResult = userManager.Create(new IdentityUser(username), password);
+            Console.WriteLine($"{creationResult.Succeeded}. {creationResult.Errors.Aggregate("Reason: ",(e, f) => e + f)}");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ AspIdentityPracticeConsole/ReadMe.md
@@ -0,0 +1,13 @@
+﻿3 Nuget Packages requried:
+
+1. Microsoft.AspNet.Identity.Core
+2. Microsoft.AspNet.Identity.EntityFramework
+3. EntityFramework
+
+
+
+In the app.config, a connection string named **DefaultConnection** is required, since EntityFramework uses it as the default to initialize the DbContext or IdentityDbContext.
+Details here: https://stackoverflow.com/questions/44474465/asp-net-identity-fails-to-connect-to-the-database-even-when-ef-can-successfully
+
+
+

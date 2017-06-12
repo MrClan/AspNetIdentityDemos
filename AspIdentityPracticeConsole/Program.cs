ee3a0174d07c018965e5468bec55c24712d05d2e
MrClan <MrClan@HomeAsusPC>
2017-06-12 05:56:28 +0545

    Module 1

--- AspIdentityPracticeConsole/Program.cs
+++ AspIdentityPracticeConsole/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Claims;
 
 namespace AspIdentityPracticeConsole
 {
@@ -21,8 +22,25 @@ namespace AspIdentityPracticeConsole
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
 
+
+            // Create new user
             var creationResult = userManager.Create(new IdentityUser(username), password);
             Console.WriteLine($"{creationResult.Succeeded}. {creationResult.Errors.Aggregate("Reason: ",(e, f) => e + f)}");
+
+            // Get user to get user's id and other properties
+            var user = userManager.FindByName(username);
+
+            // Remove claim
+            var removeClaimResult = userManager.RemoveClaim(user.Id, new Claim("given_name", "MrClan"));
+
+            // Create new claim for the user
+            var claimResult = userManager.AddClaim(user.Id, new Claim("given_name", "MrClan"));
+            Console.WriteLine($"Claim: {claimResult.Succeeded}");
+            
+            // verify user password
+            var isMatch = userManager.CheckPassword(user, password);
+            Console.WriteLine($"Password Matches: {isMatch}");
+
             Console.ReadKey();
         }
     }
